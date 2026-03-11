50d5b8254d09ba97a6f3afe9ec7672ee51dc7ee2
Barbara Bermes <bbinto@users.noreply.github.com>
2026-03-11 10:06:35 -0400

    vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
-    allowedHosts: ['hyperconscientious-mel-exhaustingly.ngrok-free.dev'],
+
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
